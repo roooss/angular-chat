40290c7503601d62793b2c0639dc0b02a539c5cc
Ubuntu <roooss@Server.paqd510rapuexiogv0blywfs1e.zx.internal.cloudapp.net>
2017-04-06 23:00:27 +0000

    routing sorted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
-    { path: 'home', component: 'HomeComponent' },
-  {
+    { path: 'home', component: HomeComponent },
+    { path: 'profile/:id', component: ProfileComponent },
+    {
     path: '',
-    redirectTo: '/home'
+    component: HomeComponent,
+    pathMatch: 'full'
   }
 ];
 
