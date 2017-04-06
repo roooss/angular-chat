15d69ec0e8d8eb2684824724de1dbf51a443849c
Ubuntu <roooss@Server.paqd510rapuexiogv0blywfs1e.zx.internal.cloudapp.net>
2017-04-06 22:36:53 +0000

    styling fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+
 const routes: Routes = [
+    { path: 'home', component: 'HomeComponent' },
   {
     path: '',
-    children: []
+    redirectTo: '/home'
   }
 ];
 
